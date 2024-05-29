f3d731a7e69dc5f640d873baf35df41ef0143d84
lai <lionlai1989@gmail.com>
2024-05-29 16:12:16 +0200

    update

--- src/zionnamespacepackage/firstpackage/__init__.py
+++ src/zionnamespacepackage/firstpackage/__init__.py
@@ -1,7 +1,8 @@
 """Package docstring. Think cautiously on putting the __version__ here. If __version__="0.1.0" is
 put here. Print firstpackage.__version__ will get the output "0.1.0"."""
 
-import subprocess, os, sysconfig, sys
+import os
+import subprocess
 
 src_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ["PATH"] = src_dir + os.pathsep + os.environ["PATH"]
